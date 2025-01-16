eae9e2f966c23e5cbca7ca07b46817b75ce57ede
imstarbucks <37726091+imstarbucks@users.noreply.github.com>
2025-01-16 12:08:11 +0800

    feat: add hooks & utils

---
+++ hooks/useWindowSize.ts
@@ -0,0 +1,30 @@
+import { useEffect, useState } from "react";
+
+const useWindowSize = () => {
+  const [windowSize, setWindowSize] = useState<{
+    width: number;
+    height: number;
+  }>({
+    width: window.innerWidth || 0,
+    height: window.innerHeight || 0,
+  });
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowSize({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    handleResize();
+
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  return windowSize;
+};
+
+export { useWindowSize };
